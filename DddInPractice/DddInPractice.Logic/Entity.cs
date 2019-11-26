0e57ccc2f49f444ab10842dbe060f42884225844
Dragos Rogojan <dragos.rogojan@gmail.com>
2019-11-27 01:28:01 +0200

    Added SnackMachine, Money and Entity.

---
+++ DddInPractice/DddInPractice.Logic/Entity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DddInPractice.Logic
+{
+    public abstract class Entity
+    {
+        public long Id { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (other.GetType() != this.GetType()) return false;
+            if (Id == 0 || other.Id == 0) return false;
+
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().ToString() + Id).GetHashCode();
+        }
+
+        public static bool operator ==(Entity left, Entity right)
+        {
+            if (left is null && right is null)
+                return true;
+
+            if (left is null || right is null)
+                return false;
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Entity left, Entity right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ DddInPractice/DddInPractice.Logic/Money.cs
@@ -0,0 +1,42 @@
+﻿namespace DddInPractice.Logic
+{
+    public sealed class Money
+    {
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set; }
+        public int QuarterCount { get; private set; }
+        public int OneDollarCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
+
+        public Money(
+            int oneCentCount,
+            int tenCentCount,
+            int quarterCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount)
+        {
+            OneCentCount += oneCentCount;
+            TenCentCount += tenCentCount;
+            QuarterCount += quarterCount;
+            OneDollarCount += oneDollarCount;
+            FiveDollarCount += fiveDollarCount;
+            TwentyDollarCount += twentyDollarCount;
+        }
+
+        public static Money operator +(Money firstAmount, Money secondAmount)
+        {
+            var sum = new Money(
+                firstAmount.OneCentCount + secondAmount.OneCentCount,
+                firstAmount.TenCentCount + secondAmount.TenCentCount,
+                firstAmount.QuarterCount + secondAmount.QuarterCount,
+                firstAmount.OneDollarCount + secondAmount.OneDollarCount,
+                firstAmount.FiveDollarCount + secondAmount.FiveDollarCount,
+                firstAmount.TwentyDollarCount + secondAmount.TwentyDollarCount
+                );
+
+            return sum;
+        }
+    }
+}
