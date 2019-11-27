0085e1f84e13348f89d1f8abcd4951b4c959414f
Dragos Rogojan <dragos.rogojan@gmail.com>
2019-11-27 23:09:47 +0200

    Added ValueObject

--- DddInPractice/DddInPractice.Logic/Money.cs
+++ DddInPractice/DddInPractice.Logic/Money.cs
@@ -1,6 +1,6 @@
 ﻿namespace DddInPractice.Logic
 {
-    public sealed class Money
+    public sealed class Money : ValueObject<Money>
     {
         public int OneCentCount { get; private set; }
         public int TenCentCount { get; private set; }
@@ -37,6 +37,31 @@
                 );
 
             return sum;
+        }
+
+        protected override bool EqualsCore(Money other)
+        {
+            return OneCentCount == other.OneCentCount
+                   && TenCentCount == other.TenCentCount
+                   && QuarterCount == other.QuarterCount
+                   && OneDollarCount == other.OneDollarCount
+                   && FiveDollarCount == other.FiveDollarCount
+                   && TwentyDollarCount == other.TwentyDollarCount;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                int hashCode = OneCentCount;
+                hashCode = (hashCode * 397) ^ TenCentCount;
+                hashCode = (hashCode * 397) ^ QuarterCount;
+                hashCode = (hashCode * 397) ^ OneDollarCount;
+                hashCode = (hashCode * 397) ^ FiveDollarCount;
+                hashCode = (hashCode * 397) ^ TwentyDollarCount;
+
+                return hashCode;
+            }
         }
     }
 }
---
+++ DddInPractice/DddInPractice.Logic/ValueObject.cs
@@ -0,0 +1,42 @@
+﻿namespace DddInPractice.Logic
+{
+    public abstract class ValueObject<T>
+        where T : ValueObject<T>
+    {
+        public override bool Equals(object obj)
+        {
+            var valueObject = obj as T;
+
+            if (ReferenceEquals(obj, null))
+                return false;
+
+            return EqualsCore(valueObject);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();  
+        }
+
+        public static bool operator ==(ValueObject<T> left, ValueObject<T> right)
+        {
+            if (left is null && right is null)
+                return true;
+
+            if (left is null || right is null)
+                return false;
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ValueObject<T> left, ValueObject<T> right)
+        {
+            return !(left == right);
+        }
+
+        protected abstract bool EqualsCore(T other);
+
+        protected abstract int GetHashCodeCore();
+
+    }
+}
